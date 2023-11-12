6e55737a6d33ade4b6f35d0efffcd633ebd8a34b
Acqua Di Giorgio <774248@unizar.es>
2023-11-12 20:49:31 +0100

    pd controller implemented, using only cX
    Linear speed reduces in curves
    (!!) Full lap in exactly 400 seconds (6 minutes and 40 seconds)
    
    TODO: use also cY

--- FollowLineController.py
+++ FollowLineController.py
@@ -2,53 +2,101 @@ from GUI import GUI
 from HAL import HAL
 import cv2
 
-def getImage():
-  img = HAL.getImage()
-  hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-  red_mask = cv2.inRange(hsv, (0, 125, 125), (30, 255, 255))
-  contours, hierarchy = cv2.findContours(red_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-  
-  return img, red_mask, contours
-
-def setSpeed(v, w):
-  HAL.setV(v)
-  HAL.setW(w)
-
-def process(contours):
-  M = cv2.moments(contours[0])
-  
-  cX, cY = 0, 0
-  if M["m00"] != 0:
-    cX = M["m10"] / M["m00"]
-    cY = M["m01"] / M["m00"]
+class Robot():
+  def __init__(self):
+    self.prev_error = 0
     
-  if cX > 0:
-    err = 320 - cX
-    setSpeed(2, 0.01*err)
+    self.prev_cX = 0
+    self.prev_cY = 0
+    self.iters_motionless = 0
+    
+    self.KP = 0.05
+    self.KD = 0.025
+    
+    self.v = 2
+    self.max_w = 1
+    
   
-  return cX, cY
+  def set_angular_Naive(self, w):
+    HAL.setV(self.v)
+    HAL.setW(w)
+    
+    return self.v
+    
+    
+  def set_angular(self, w):
+    v = (self.max_w - abs(w)) * self.v
+    
+    HAL.setV(v)
+    HAL.setW(w)
+    
+    return v
+    
+    
+  # cY = 398.29
+  def setSpeed_cXY(self, cX, cY):
+    error_p = 320 - cX
+    error_d = error_p - self.prev_error
+    
+    self.prev_error = error_p
+    w = 0.02 * (error_p * self.KP + error_d * self.KD)
+    
+    if w > self.max_w:
+      w = self.max_w
+    elif w < -self.max_w:
+      w = -self.max_w
+    
+    print("error_p:", error_p)
+    print("error_d:", error_d)
+    print("previous:", self.prev_error)
+    
+    v = self.set_angular(w)
+    print("Linear speed:", v, "Angular speed:", w)
+    
   
-i = 0
-last_cX = 0
-last_cY = 0
-sameC = 0
-
-while True:
-    img, red_mask, contours = getImage()  
-    cX, cY = process(contours)
+  def process(self, i):
+    img = HAL.getImage()
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    red_mask = cv2.inRange(hsv, (0, 125, 125), (30, 255, 255))
+    contours, hierarchy = cv2.findContours(red_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    
+    M = cv2.moments(contours[0])
+    
+    cX, cY = 0, 0
+    if M["m00"] != 0:
+      cX = M["m10"] / M["m00"]
+      cY = M["m01"] / M["m00"]
+    
+    self.setSpeed_cXY(cX, cY)
+      
+    # if cX > 0:
+    #   err = 320 - cX
+    #   self.setSpeed(2, 0.01*err)
+    
+    print('%d) cX: %.2f  cY: %.2f' % (i, cX, cY))
+    print()
     GUI.showImage(red_mask)
     
-    print('%d -> cX: %.2f  cY: %.2f' % (i, cX, cY))
+    self.check_motion(cX, cY)
     
-    # Reseting Purpose
-    if cX == last_cX and cY == last_cY:
-      sameC = sameC + 1
-      if sameC > 20:
-        setSpeed(0, 0)
+    
+  def check_motion(self, cX, cY):
+    if cX == self.prev_cX and cY == self.prev_cY:
+      self.iters_motionless += 1
+      
+      if self.iters_motionless > 20:
         exit()
-    else:
-      sameC = 0
     
-    last_cX = cX
-    last_cY = cY
-    i = i + 1
+    else:
+      self.iters_motionless = 0
+    
+    self.prev_cX = cX
+    self.prev_cY = cY
+    
+    
+i = 0
+rob = Robot()
+
+while True:
+    rob.process(i)
+    i += 1
